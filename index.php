42ea4b6181deec98626111e956fef7dedde54232
filon13p <filon13p@gmail.com>
2020-12-06 19:37:24 +0300

    Web4

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Авторизация пользователя</title>
+</head>
+
+
+<body>
+
+    <form action="authorize.php" method="post">
+        Имя:<input type="text" name="user_name"> Пароль:
+        <input type="password" name="user_pass">
+        <input type="submit" name="Submit">
+    </form>
+
+</body>
+
+
+</html>
